044c88c5586dcb6fb97c162971eda23cd3ae7c6e
Konrad Bujas <kondziobu@gmail.com>
2023-01-16 15:57:40 +0100

    routing: dashboard & detali view - step 1

--- app.js
+++ app.js
@@ -1,58 +1,14 @@
-import { renderCountriesList } from "./dom-utils.js";
 
-console.log('Hey there ! ;)');
+import { renderDashboard } from "./view-dashboard.js";
+import { renderDetail } from "./view-detail.js";
+
+console.log('Hey there! ;)');
 
 if (window.location.search.includes("?country=")) {
     console.log("routing learning work perfect ! ;D");
-} else {
-
-
-const API_URL_ALL = "https://restcountries.com/v3.1/all";
-
-let countries;
-let query = "";
-let region = "";
-
-fetch(API_URL_ALL)
-    .then(res => res.json())
-    .then((countriesRaw) => {
-        // console.log(countriesRaw);
-        countries = countriesRaw.filter((country) => country.name.common.toLowerCase().includes(query.toLowerCase()));
-        countries = countries.map((country) => {
-            // if (country.name.common === "Australia") {console.log(country.region)};
-            return {
-                capital: country.capital && country.capital[0],
-                population: country.population.toLocaleString(),
-                name: country.name.common,
-                region: country.region,
-                flagUrl: country.flags.png,
-            };
-        });
-        // console.log(countries);
-    renderCountriesList(countries);
-    });
     
-const filteredDataAndRenderCountriesList = () => {
-    const filteredCountries = countries.filter((country) => {
-        return (
-            country.name.toLowerCase().includes(query) &&
-            (!region || country.region === region)
-        );
-    });
-    renderCountriesList(filteredCountries);
+    renderDetail();
+} else {
+    document.querySelector(".filters").classList.add("active");
+    renderDashboard();
 };
-
-
-document.querySelector('#query').addEventListener("input", (e) => {
-    // console.log(e.target.value);
-    query = e.target.value.toLowerCase().trim();
-    filteredDataAndRenderCountriesList();
-});
-
-document.querySelector('#region').addEventListener("change", (e) => {
-    // console.log(e.target.value);
-    region = e.target.value;
-    filteredDataAndRenderCountriesList();   
-});
-
-}
---
+++ view-dashboard.js
@@ -0,0 +1,52 @@
+import { renderCountriesList } from "./dom-utils.js";
+
+export const renderDashboard = () => {
+    const API_URL_ALL = "https://restcountries.com/v3.1/all";
+
+    let countries;
+    let query = "";
+    let region = "";
+    
+    fetch(API_URL_ALL)
+        .then(res => res.json())
+        .then((countriesRaw) => {
+            // console.log(countriesRaw);
+            countries = countriesRaw.filter((country) => country.name.common.toLowerCase().includes(query.toLowerCase()));
+            countries = countries.map((country) => {
+                // if (country.name.common === "Australia") {console.log(country.region)};
+                return {
+                    capital: country.capital && country.capital[0],
+                    population: country.population.toLocaleString(),
+                    name: country.name.common,
+                    region: country.region,
+                    flagUrl: country.flags.png,
+                };
+            });
+            // console.log(countries);
+        renderCountriesList(countries);
+        });
+        
+    const filteredDataAndRenderCountriesList = () => {
+        const filteredCountries = countries.filter((country) => {
+            return (
+                country.name.toLowerCase().includes(query) &&
+                (!region || country.region === region)
+            );
+        });
+        renderCountriesList(filteredCountries);
+    };
+    
+    
+    document.querySelector('#query').addEventListener("input", (e) => {
+        // console.log(e.target.value);
+        query = e.target.value.toLowerCase().trim();
+        filteredDataAndRenderCountriesList();
+    });
+    
+    document.querySelector('#region').addEventListener("change", (e) => {
+        // console.log(e.target.value);
+        region = e.target.value;
+        filteredDataAndRenderCountriesList();   
+    });
+
+};
